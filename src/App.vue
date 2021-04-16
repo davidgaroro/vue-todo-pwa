2ec4a6526b9f45b6f538e0136ac58ae887da9670
davidgaroro <davidgaroro@gmail.com>
2021-04-16 19:42:03 +0200

    add footer component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,18 @@
   <main class="container">
     <router-view />
   </main>
+  <Footer />
 </template>
+
+<script>
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    Footer,
+  },
+};
+</script>
 
 <style lang="scss">
 .container {
---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer class="small mt-5 text-secondary">
+    <p class="mb-2">
+      Made with
+      <a
+        class="link-secondary"
+        href="https://v3.vuejs.org"
+        target="_blank"
+        rel="noopener"
+        >Vue 3</a
+      >
+      and
+      <a
+        class="link-secondary"
+        href="https://getbootstrap.com"
+        target="_blank"
+        rel="noopener"
+        >Bootstrap 5</a
+      >
+    </p>
+    <p>
+      <a
+        class="link-secondary"
+        href="https://github.com/davidgaroro/vue-todo-pwa"
+        target="_blank"
+        rel="noopener"
+        >View on GitHub</a
+      >
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
