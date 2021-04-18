6aefa61d0408840a0d466efc1d8bdce394e25365
davidgaroro <davidgaroro@gmail.com>
2021-04-18 02:19:58 +0200

    add files to production

---
+++ precache-manifest.c58112c95914590f00917c50e95da38c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8200a586631a7fc63d",
+    "url": "/vue-todo-pwa/css/app.b0546bb2.css"
+  },
+  {
+    "revision": "ca266facb9db5e6eae70",
+    "url": "/vue-todo-pwa/css/chunk-vendors.0f1ada3b.css"
+  },
+  {
+    "revision": "06db1ca907da2e9ae0d129dc4d954d22",
+    "url": "/vue-todo-pwa/index.html"
+  },
+  {
+    "revision": "5a8200a586631a7fc63d",
+    "url": "/vue-todo-pwa/js/app.594d9d2d.js"
+  },
+  {
+    "revision": "ca266facb9db5e6eae70",
+    "url": "/vue-todo-pwa/js/chunk-vendors.a1a1f0d8.js"
+  },
+  {
+    "revision": "62a0b948a9069cbde3273758fabcd5d0",
+    "url": "/vue-todo-pwa/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-todo-pwa/robots.txt"
+  }
+]);
